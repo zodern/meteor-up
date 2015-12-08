830f2a86e42fab069600c6238e72244e7f1ad724
Muhammed Thanish <mnmtanish@gmail.com>
2015-12-08 17:02:06 +0530

    Add docker module

---
+++ lib/modules/docker/assets/docker-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# TODO server must be Ubuntu 14.04.x LTS version
+# TODO make sure we can run docker in this server
+
+# Is docker already installed?
+set +e
+hasDocker=$(docker version | grep "version")
+set -e
+
+if [ ! "$hasDocker" ]; then
+  # Remove the lock
+  set +e
+  sudo rm /var/lib/dpkg/lock > /dev/null
+  sudo rm /var/cache/apt/archives/lock > /dev/null
+  sudo dpkg --configure -a
+  set -e
+
+  # Required to update system
+  sudo apt-get update
+  sudo apt-get -y install wget lxc iptables
+
+  # Install docker
+  wget -qO- https://get.docker.com/ | sudo sh
+  sudo usermod -a -G docker ${USER}
+  service docker start
+fi
+
+# TODO make sure docker works as expected
