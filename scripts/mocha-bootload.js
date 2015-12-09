6dd8e4d05fb8c468142498ca50ba5fbb0a34f38a
Muhammed Thanish <mnmtanish@gmail.com>
2015-12-09 14:31:56 +0530

    Add a __rootdir global

--- scripts/mocha-bootload.js
+++ scripts/mocha-bootload.js
@@ -1,5 +1,10 @@
 require('babel-core/register');
 
+// add a __rootdir global to avoid '../../../../../'
+// like paths when requiring package files.
+var path = require('path');
+__rootdir = path.resolve(__dirname, '..');
+
 process.on('unhandledRejection', function (error) {
   console.error('Unhandled Promise Rejection:');
   console.error(error && error.stack || error);
