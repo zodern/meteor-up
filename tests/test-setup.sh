44854394b71c90f10a597cfb6d02304aa0269493
Madushan Nishantha <j.l.madushan@gmail.com>
2015-12-21 16:13:48 +0530

    splited test-run.sh

---
+++ tests/test-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install -y nodejs 
+wget -qO- https://get.docker.com/ | sudo sh 
+sudo service docker start 
+cd ~/meteor-up/tests/  
+ssh-keygen -f new -t rsa -N '' 
+docker build -t mybase . 
+sudo docker run --privileged=true -d --name 'mydoc' -t mybase /sbin/my_init
