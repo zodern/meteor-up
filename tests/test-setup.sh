c1e0bfc8e486f1acecc26f4bf261bba070b12404
Madushan Nishantha <j.l.madushan@gmail.com>
2015-12-21 16:20:18 +0530

    docker cleanup before tests

--- tests/test-setup.sh
+++ tests/test-setup.sh
@@ -5,6 +5,7 @@ sudo apt-get install -y nodejs
 wget -qO- https://get.docker.com/ | sudo sh 
 sudo service docker start 
 cd ~/meteor-up/tests/  
-ssh-keygen -f new -t rsa -N '' 
+ssh-keygen -f new -t rsa -N ''
+sudo docker rm -f mydoc
 docker build -t mybase . 
 sudo docker run --privileged=true -d --name 'mydoc' -t mybase /sbin/my_init
