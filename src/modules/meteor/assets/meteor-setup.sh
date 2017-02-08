e100ab9baf955cbae82e3933252e9e121814479b
Shannon Duncan <jsduncan98@gmail.com>
2017-02-08 16:03:26 -0600

    add nginx-config to app area

--- src/modules/meteor/assets/meteor-setup.sh
+++ src/modules/meteor/assets/meteor-setup.sh
@@ -3,4 +3,9 @@
 sudo mkdir -p /opt/<%= name %>/
 sudo mkdir -p /opt/<%= name %>/config
 sudo mkdir -p /opt/<%= name %>/tmp
+sudo cat <<EOT >> /opt/config/nginx-default.conf
+{
+  client_max_body_size 100m;
+}
+EOT
 sudo chown ${USER} /opt/<%= name %> -R
