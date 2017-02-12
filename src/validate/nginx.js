75b52dea5b1e8547dce29d988f9b70deed6d9236
Shai Amir <shai.amir1@gmail.com>
2017-02-12 22:06:53 +0200

    Added validation for nginx and some bug fixes from the merge.

---
+++ src/validate/nginx.js
@@ -0,0 +1,40 @@
+/**
+ * @author Shai Amir
+ */
+
+import { addLocation, combineErrorDetails, serversExist } from './utils';
+
+import joi from 'joi';
+
+const schema = joi.object().keys({
+  name: joi.string().min(1).required(),
+  httpPort: joi.number(),
+  httpsPort: joi.number(),
+  env: joi
+    .object()
+    .keys({
+      DEFAULT_HOST: joi.string()
+    })
+    .pattern(/[\s\S]*/, [joi.string(), joi.number(), joi.boolean()]),
+  env: joi
+    .object()
+    .keys({
+      ACME_CA_URI: joi
+        .string()
+        .regex(new RegExp('^(http|https)://', 'i')),
+      DEBUG: joi.boolean(),
+      NGINX_PROXY_CONTAINER: joi.string()
+    })
+    .pattern(/[\s\S]*/, [joi.string(), joi.number(), joi.boolean()]),
+  servers: joi.object().required()
+});
+
+export default function(config) {
+  let details = [];
+  details = combineErrorDetails(details, joi.validate(config.nginx, schema));
+  details = combineErrorDetails(
+    details,
+    serversExist(config.servers, config.nginx.servers)
+  );
+  return addLocation(details, 'nginx');
+}
