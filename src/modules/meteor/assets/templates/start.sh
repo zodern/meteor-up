24d43424004ec957c0edde996d4bb3567f8b8ff4
zodern <zodern@users.noreply.github.com>
2017-03-03 19:09:26 -0600

    Fix nginx-proxy container not starting on server restart

--- src/modules/meteor/assets/templates/start.sh
+++ src/modules/meteor/assets/templates/start.sh
@@ -58,13 +58,13 @@ sleep 15s
     echo "Running autogenerate"
     # Get the nginx template for nginx-gen
     wget https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl -O /opt/$APPNAME/config/nginx.tmpl
-    
+
     # Update nginx config based on user input or default passed by js
 sudo cat <<EOT > /opt/$APPNAME/config/nginx-default.conf
 client_max_body_size $CLIENTSIZE;
 EOT
-    
-    
+
+
     # We don't need to fail the deployment because of a docker hub downtime
     set +e
     docker pull jrcs/letsencrypt-nginx-proxy-companion:latest
@@ -74,6 +74,7 @@ EOT
     echo "Pulled autogenerate images"
     docker run -d -p 80:80 -p 443:443 \
       --name $APPNAME-nginx-proxy \
+      --restart=always \
       -e "HTTPS_METHOD=noredirect" \
       -v /opt/$APPNAME/config/nginx-default.conf:/etc/nginx/conf.d/my_proxy.conf:ro \
       -v /opt/$APPNAME/certs:/etc/nginx/certs:ro \
