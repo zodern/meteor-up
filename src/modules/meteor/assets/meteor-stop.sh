b591dc845037400794ac66105a6a53274df5633a
Patrick Schubert <patrick.schubert1@gmail.com>
2017-03-06 18:47:05 +0100

    Update meteor-stop.sh

--- src/modules/meteor/assets/meteor-stop.sh
+++ src/modules/meteor/assets/meteor-stop.sh
@@ -2,5 +2,7 @@
 
 APPNAME=<%= appName %>
 
+sudo docker network disconnect -f bridge $APPNAME || :
 sudo docker rm -f $APPNAME || :
+sudo docker network disconnect -f bridge $APPNAME-frontend || :
 sudo docker rm -f $APPNAME-frontend || :
