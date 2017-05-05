8ffe053579a4438ba181844d174b927bf206cfb7
zodern <zodern@users.noreply.github.com>
2017-05-04 23:19:42 -0500

    Deploy check follow redirects

--- src/modules/meteor/assets/meteor-deploy-check.sh
+++ src/modules/meteor/assets/meteor-deploy-check.sh
@@ -26,10 +26,11 @@ elaspsed=0
 while [[ true ]]; do
   sleep 1
   elaspsed=$((elaspsed+1))
-  curl --fail localhost:$DEPLOY_CHECK_PORT && exit 0
+  curl --fail -L localhost:$DEPLOY_CHECK_PORT && exit 0
 
   if [ "$elaspsed" == "$DEPLOY_CHECK_WAIT_TIME" ]; then
     revert_app
     exit 1
   fi
 done
+ 
