9df4216f713dff9b36c4a4c28c8263f57a2d8705
zodern <zodern@users.noreply.github.com>
2017-05-14 21:10:05 -0500

    Update proxy validator

--- src/validate/proxy.js
+++ src/validate/proxy.js
@@ -1,32 +1,40 @@
-import { addLocation, combineErrorDetails, serversExist } from './utils';
+import {
+  VALIDATE_OPTIONS,
+  addLocation,
+  combineErrorDetails,
+  serversExist
+} from './utils';
 
 import joi from 'joi';
 
 const schema = joi.object().keys({
-  name: joi.string().min(1).required(),
-  httpPort: joi.number(),
-  httpsPort: joi.number(),
+  servers: joi.object().required(),
+  ssl: joi.object(),
+  domains: joi.string().required(),
   clientUploadLimit: joi.number(),
-  env: joi
-    .object()
-    .keys({
-      DEFAULT_HOST: joi.string()
-    })
-    .pattern(/[\s\S]*/, [joi.string(), joi.number(), joi.boolean()]),
-  env: joi
-    .object()
-    .keys({
-      ACME_CA_URI: joi.string().regex(new RegExp('^(http|https)://', 'i')),
-      DEBUG: joi.boolean(),
-      NGINX_PROXY_CONTAINER: joi.string()
-    })
-    .pattern(/[\s\S]*/, [joi.string(), joi.number(), joi.boolean()]),
-  servers: joi.object().required()
+  shared: joi.object().keys({
+    httpPort: joi.number(),
+    httpsPort: joi.number(),
+    env: joi
+      .object()
+      .pattern(/[\s\S]*/, [joi.string(), joi.number(), joi.boolean()]),
+    envLetsEncrypt: joi
+      .object()
+      .keys({
+        ACME_CA_URI: joi.string().regex(new RegExp('^(http|https)://', 'i')),
+        DEBUG: joi.boolean(),
+        NGINX_PROXY_CONTAINER: joi.string()
+      })
+      .pattern(/[\s\S]*/, [joi.string(), joi.number(), joi.boolean()])
+  })
 });
 
 export default function(config) {
   let details = [];
-  details = combineErrorDetails(details, joi.validate(config.proxy, schema));
+  details = combineErrorDetails(
+    details,
+    joi.validate(config.proxy, schema, VALIDATE_OPTIONS)
+  );
   if (config.proxy.servers) {
     details = combineErrorDetails(
       details,
