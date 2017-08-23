b304ae0ed9d5795ebc80a2cbb4a41fd8d7a90af9
Tyler Sowers <tyler@startrakstudios.com>
2017-08-23 14:41:24 -0400

    Fix issue with ZSH (#530)
    * Quote equality operator
    
    * Add Shebang

--- src/plugins/meteor/assets/meteor-deploy-check.sh
+++ src/plugins/meteor/assets/meteor-deploy-check.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 APPNAME=<%= appName %>
 APP_PATH=/opt/$APPNAME
 IMAGE=mup-<%= appName %>
@@ -48,7 +50,7 @@ while [[ true ]]; do
     <% if (host) { %> --header "HOST:$HOST" <% } %>  \
     && exit 0 
 
-  if [ "$elaspsed" == "$DEPLOY_CHECK_WAIT_TIME" ]; then
+  if [ "$elaspsed" "==" "$DEPLOY_CHECK_WAIT_TIME" ]; then
     revert_app
     exit 1
   fi
