363ff21e944aee7edbb4ef4023bf848f11905f61
zodern <zodern@users.noreply.github.com>
2017-09-12 18:01:16 -0500

    Fix unit test

--- src/__tests__/plugin-api.unit.js
+++ src/__tests__/plugin-api.unit.js
@@ -166,7 +166,7 @@ describe('PluginAPI', () => {
 
   describe('_normalizeConfig', () => {
     it('should copy meteor object to app', () => {
-      const expected = { meteor: { path: '../' }, app: { type: 'meteor', path: '../' } };
+      const expected = { meteor: { path: '../' }, app: { type: 'meteor', path: '../', docker: { image: 'kadirahq/meteord' } } };
       const config = { meteor: { path: '../' } };
       const result = api._normalizeConfig(config);
 
