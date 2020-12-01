755c077e5883ed1145e2bafc2127776c0a6aa10b
zodern <zodern@users.noreply.github.com>
2020-12-01 13:43:52 -0600

    Show warning on Node 14

--- src/node-version.js
+++ src/node-version.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-var */
 // This file doesn't use es6 since it needs to work on old versions of Node
 
 if (typeof Buffer.alloc === 'undefined') {
@@ -5,11 +6,22 @@ if (typeof Buffer.alloc === 'undefined') {
   process.exit(1);
 }
 
+var versionParts = process.versions.node.split('.');
+var version = parseInt(versionParts[0], 10);
 // eslint-disable-next-line no-var
-var version = parseInt(process.versions.node.split('.')[0], 10);
+var minorVersion = parseInt(versionParts[1], 10);
 
 if (version < 8) {
   // eslint-disable-next-line
   console.log('Meteor Up requires node 8 or newer. You are using ' + process.version);
   process.exit(1);
+}
+
+if (version === 14 && minorVersion <= 15) {
+  // eslint-disable-next-line global-require
+  var chalk = require('chalk');
+
+  console.log(chalk.yellow('---------------------------------------------------------------------------'));
+  console.warn(chalk.yellow('Node 14.1.0 - 14.15 is not supported. Please use a different Node version.'));
+  console.log(chalk.yellow('---------------------------------------------------------------------------'));
 }
