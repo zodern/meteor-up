d1967213780b3bf6529f5e84175801db72918927
zodern <zodern@users.noreply.github.com>
2021-09-30 14:38:36 -0500

    Pass password as argument to docker

--- src/plugins/meteor/prepare-bundle.js
+++ src/plugins/meteor/prepare-bundle.js
@@ -77,7 +77,10 @@ export async function prepareBundleLocally(
 
   console.log('');
   console.log('=> Updating base image');
-  await runCommand('docker', ['login', '--password-stdin', '--username', privateDockerRegistry.username, privateDockerRegistry.host], { stdin: privateDockerRegistry.password });
+  await runCommand(
+    'docker',
+    ['login', '--username', privateDockerRegistry.username, '--password', privateDockerRegistry.password, privateDockerRegistry.host]
+  );
   await runCommand('docker', ['pull', appConfig.docker.image]);
 
   console.log('');
