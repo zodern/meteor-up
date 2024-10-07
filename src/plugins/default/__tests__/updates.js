2f2acea41050c24b34cd5ea31f271858cc503e60
zodern <zodern@users.noreply.github.com>
2024-10-07 11:49:19 -0500

    Remove .only

--- src/plugins/default/__tests__/updates.js
+++ src/plugins/default/__tests__/updates.js
@@ -2,7 +2,7 @@ import { describe, it } from 'mocha';
 import { checkPackageUpdates } from '../../../updates';
 import { expect } from 'chai';
 
-describe.only('package updates', () => {
+describe('package updates', () => {
   it('should detect update', async () => {
     const result = await checkPackageUpdates('mup', {version: '1.0.0'});
     expect(result).to.equal(true);
