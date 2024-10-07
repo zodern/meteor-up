22f8ef478df2c64f3973f9198db51b6c7470790f
zodern <zodern@users.noreply.github.com>
2024-10-07 10:10:32 -0500

    Add basic updates test

---
+++ src/plugins/default/__tests__/updates.js
@@ -0,0 +1,10 @@
+import { describe, it } from 'mocha';
+import { checkPackageUpdates } from '../../../updates';
+import { expect } from 'chai';
+
+describe.only('package updates', () => {
+  it('should detect update', async () => {
+    const result = await checkPackageUpdates('mup', {version: '1.0.0'});
+    expect(result).to.equal(true);
+  });
+});
